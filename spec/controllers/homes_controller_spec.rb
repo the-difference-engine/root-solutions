39c89f495f327361e69456bd83a7e86354caf2e6
Anna Natorilla <annagaiel@gmail.com>
2016-07-10 10:05:00 -0700

    wrote the first test for index action

--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe HomesController, type: :controller do
-
+  describe "homes#index action" do
+    it "should successfully show the page" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
