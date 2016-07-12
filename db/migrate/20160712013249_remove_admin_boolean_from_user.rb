4d0e64ba211b5e658acc91a6f7f1043eb329413d
Andrew Wei <andrew.wei92@yahoo.com>
2016-07-11 18:34:51 -0700

    Removed admin boolean value from user model

---
+++ db/migrate/20160712013249_remove_admin_boolean_from_user.rb
@@ -0,0 +1,5 @@
+class RemoveAdminBooleanFromUser < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :users, :admin, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711044201) do
+ActiveRecord::Schema.define(version: 20160712013249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 20160711044201) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.boolean  "admin"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
