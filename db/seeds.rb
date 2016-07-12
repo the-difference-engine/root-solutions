f271b5fc624cccf0c186601fac102b4aa6e7aeea
Oliver <deftool@outlook.com>
2016-07-11 19:57:33 -0700

    created seed file for admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+#create sample admin user
+
+admin = User.new
+admin.email = 'admin@admin.com'
+admin.password = 'password'
+admin.password_confirmation = 'password'
+admin.save!
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
