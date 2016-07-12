15b983e996fdb60052100a3387711d9128c3a814
nordgaard <jenordgaard@gmail.com>
2016-07-11 20:00:03 -0700

    Initial commit, Model and Controller for Tag Created

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,22 @@
+class TagsController < ApplicationController
+
+  def show
+  end
+
+  def index
+  end
+
+  def new 
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   get '/', to: 'homes#index'
+
+  resorces: tags
+
 end
