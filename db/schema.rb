c257fe317494b151934e5e5aff1c8f52c927a066
nordgaard <jenordgaard@gmail.com>
2016-07-13 11:15:32 -0700

    Initial commit for tag model, model, controller, and view page made, only index method so far

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711044201) do
+ActiveRecord::Schema.define(version: 20160713180701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
