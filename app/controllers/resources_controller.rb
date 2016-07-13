13ec265ee86d91c8da3fdfa8a2f3aaeb53625040
Anna Natorilla <annagaiel@gmail.com>
2016-07-12 21:01:30 -0700

    Create resource action on controller and

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,15 +1,27 @@
 class ResourcesController < ApplicationController
 
   def index
-
+    @resource = Resource.all
   end
 
   def new
-
+    @resource = Resource.new
   end
 
   def show
-
+    @resource = Resource.find_by(id: params[:id])
   end
-  
+
+  def create
+    @resource = Resource.new(resources_params)
+    @resource.save
+    redirect_to @resource
+  end
+
+  private
+
+  def resources_params
+    params.require(:resource).permit(:world_region, :title, :author,
+    :news_source, :date, :summary, :url, :notes)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'homes#index'
-
-  get '/resources', to: 'resources#index'
-  post '/resources', to: 'resources#create'
-  get '/resources/new', to: 'resources#new'
-  get '/resources/:id', to: 'resources#show'
+  resources :resources
 end
