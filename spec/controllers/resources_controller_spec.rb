79275c634bb1f60634a473c484c8a690f81c3bb0
Anna Natorilla <annagaiel@gmail.com>
2016-07-13 18:03:30 -0700

    Add test on the create resource

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe ResourcesController, type: :controller do
-  describe "grams#index action" do
-     it "should successfully show the page" do
-       get :index
-       expect(response).to have_http_status(:success)
-     end
-   end
+  describe "resources GET #index action" do
+    it "should successfully show the page" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "resources GET #new action" do
+    it "should successfully show the new form" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "resources POST #create action" do
+    it "should successfully create a new resource in the database" do
+      post :create, resource: { world_region: "USA", title: "Happy News", author: "Anna",
+       news_source: "CNN", date: "2016-01-01", summary: "Happy summary", url: "happy.com", notes: "Cheer up" }
+      resource = Resource.last
+      expect(response).to redirect_to "/resources/#{resource.id}"
+      expect(resource.title).to eq("Happy News")
+    end
+  end
 end
--- spec/factories/resources.rb
+++ spec/factories/resources.rb
@@ -1,12 +1,13 @@
 FactoryGirl.define do
   factory :resource do
-    world_region "MyString"
-    title "MyString"
-    author "MyString"
-    news_source "MyString"
-    date "2016-07-11"
-    summary "MyText"
-    url "MyString"
-    notes "MyText"
+    world_region { Faker::Address.country }
+    title { Faker::Book.title }
+    author { Faker::Book.author }
+    news_source { Faker::University.name }
+    t = Time.now
+    date "#{t.year}-#{t.month}-#{t.day}"
+    summary { Faker::Hacker.say_something_smart }
+    url { Faker::Internet.url }
+    notes { Faker::Superhero.name }
   end
 end
