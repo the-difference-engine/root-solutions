1f537a7bf64083329d0a7d909fde5219cdc60420
Andrew Wei <andrew.wei92@yahoo.com>
2016-07-19 10:06:50 -0700

    Renamed building block in Building Blocks table to name

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719170236) do
+ActiveRecord::Schema.define(version: 20160719170603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "building_blocks", force: :cascade do |t|
-    t.string   "building_block"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "resources", force: :cascade do |t|
