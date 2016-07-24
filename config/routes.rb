f6cbfafabf5b068dc6a65ebec4be1329443c93f5
Andrew Wei <andrew.wei92@yahoo.com>
2016-07-24 16:33:39 -0400

    Created alias export resource path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'homes#index'
 
-  get '/resources/export', to: 'resources#export_resources', as: :export_resources_path
+  get '/resources/export', to: 'resources#export_resources', as: :export_resources
   resources :resources
   resources :tags
   resources :building_blocks
