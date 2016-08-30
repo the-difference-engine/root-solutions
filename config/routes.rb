0a2b4643f53ddbee01fb636b2f694caf0683729f
Anna Natorilla <annagaiel@gmail.com>
2016-08-29 19:42:13 -0700

    Remove the extra devise user

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,6 @@ Rails.application.routes.draw do
       post :import
     end
   end
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'homes#index'
 
