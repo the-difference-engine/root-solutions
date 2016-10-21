9d45bf584c88a0616b5e59a1cbafbbb1382c7965
Bhanu <bhanusruti512@gmail.com>
2016-10-20 19:52:39 -0700

    changed work with us route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  root to: 'pages#about_us'
-  get '/work_with_us', 'pages#work_with_us' 
+  root to: 'pages#about_us', as: :about_us
+  get '/work_with_us', to: 'pages#work_with_us', as: :work_with_us
 
   resources :news_sources
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
