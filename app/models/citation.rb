59d5151e63d82981aeba43c3e39c0be17f2b8ffb
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-12-07 21:06:49 -0800

    add associations

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -1,2 +1,4 @@
 class Citation < ApplicationRecord
+  belongs_to :resource
+  belongs_to :news_source
 end
--- app/models/news_source.rb
+++ app/models/news_source.rb
@@ -1,3 +1,4 @@
-  class NewsSource < ApplicationRecord
+class NewsSource < ApplicationRecord
   has_many :resources
+  has_many :citations
 end
