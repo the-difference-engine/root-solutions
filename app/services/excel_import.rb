c5a62c7e6c58ae3a1f601b616cb760e97204671e
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-12-11 16:59:12 -0800

    mid process on the start of the feature branch

--- app/services/excel_import.rb
+++ app/services/excel_import.rb
@@ -1,3 +1,17 @@
-class ExcelImport
+require 'roo'
 
+class ExcelImport
+  def initialize(file)
+    @spreadsheets = Roo::Spreadsheet.open(file)
+    @spread = @spreadsheets.sheet(1)
+    @keys = @spreadsheets.sheet(2)
+  end
+
+  def key_import
+
+  end
+
+  def data_import
+
+  end
 end
