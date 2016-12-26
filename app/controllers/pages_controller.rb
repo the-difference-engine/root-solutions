298d8823927c43c36b92e9fcdf82ccad227651f6
Marisha Sahay <marishasahay@gmail.com>
2016-12-25 20:21:25 -0800

    Add flash message in controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,16 +14,17 @@ class PagesController < ApplicationController
     @subject = params[:subject]
     @message = params[:message]
     UserMailer.work_with_us_email(@name,@email,@subject,@message).deliver_now
+    flash[:success] = "Your message has been sent"
+    redirect_to "/work_with_us"
   end
 
   def careers
   end
 
-  def partners
-    
+  def partners   
   end
-  def learn 
 
+  def learn 
   end
 
 end
