1dd489b61391a91ce94c0ecbc647e978dffee2e8
Andrealiz Afable <dreafable@gmail.com>
2017-01-03 15:32:23 -0800

    add styling for mission-picture. setup keys in env, add twitter section on mission page

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,12 +1,25 @@
 require 'rubygems'
 require 'bundler/setup'
-
 require 'twitter'
 require 'json'
 
-twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = ENV['CONSUMER_KEY']
-  config.consumer_secret = ENV['CONSUMER_SECRET']
-  config.access_token = ENV['YOUR_ACCESS_TOKEN']
-  config.access_token_secret = ENV['YOUR_ACCESS_SECRET']
+class TwitterApi
+  def self.our_public_tweets
+    client.user_timeline('RootsSolutions', count: 5, exlude_replies: true, include_rts: false)
+  end
+
+  def self.client
+    @client = Twitter::Streaming::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+    end
+  end
 end
+
+# Insert the code below to Mission Page
+
+# <% TwitterApi.our_public_tweets.each do |tweet| %>
+#   <%= tweet.text %>
+# <% end %>
