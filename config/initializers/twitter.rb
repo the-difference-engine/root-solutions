782f8b8bd22b077bf9c7607e7fd8aff68e34e2b5
Andrealiz Afable <dreafable@gmail.com>
2017-01-04 19:23:50 -0800

    fix twitter api error, add tweet text to page

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -5,11 +5,11 @@ require 'json'
 
 class TwitterApi
   def self.our_public_tweets
-    client.user_timeline('RootsSolutions', count: 5, exlude_replies: true, include_rts: false)
+    client.user_timeline('Root_Solutions', count: 5, exlude_replies: true, include_rts: false)
   end
 
   def self.client
-    @client = Twitter::Streaming::Client.new do |config|
+    @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
       config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
       config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
@@ -17,6 +17,3 @@ class TwitterApi
     end
   end
 end
-
-# Insert the code below to Mission Page
-
