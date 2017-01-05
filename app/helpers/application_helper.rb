a3de22e40fc215c3b0e2fcb185e409d85f944e78
Andrealiz Afable <dreafable@gmail.com>
2017-01-04 20:22:33 -0800

    use gsub to replace display links to working hyperlinks, add target blank

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,19 @@
 module ApplicationHelper
 # Uncomment after integrating Twitter API authentication code
 
-  def parsed_tweet parsed_tweet
-    _parsed_tweet = tweet.text.dup
+  # def parsed_tweet parsed_tweet
+  #   _parsed_tweet = tweet.text.dup
 
-    tweet.urls.each do |entity|
-      html_link = link_to(entity.display_url.to_s, entity.expanded_url.to_s, target: 'blank')
-      _parsed_tweet.sub!(entity.url.to_s, html_link)
-    end
-    tweet.media.each do |entity|
-      html_link = link_to(entity.display_url.to_s, entity.expanded_url.to_s, target: 'blank')
-      _parsed_tweet.sub!(entity.url.to_s, html_link)
-    end
+  #   tweet.urls.each do |entity|
+  #     html_link = link_to(entity.display_url.to_s, entity.expanded_url.to_s, target: 'blank')
+  #     _parsed_tweet.sub!(entity.url.to_s, html_link)
+  #   end
+  
+  #   tweet.media.each do |entity|
+  #     html_link = link_to(entity.display_url.to_s, entity.expanded_url.to_s, target: 'blank')
+  #     _parsed_tweet.sub!(entity.url.to_s, html_link)
+  #   end
 
-    _parsed_tweet.html_safe
-  end
+  #   _parsed_tweet.html_safe
+  # end
 end
