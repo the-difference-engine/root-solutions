157fafae8c2cbbaca7f0624af270328a9f384507
O'Hoon Kwon <ohkwon@ucdavis.edu>
2017-01-16 20:17:33 -0800

    add controller to interact with api, basic functinoality

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -17,4 +17,16 @@ class SearchesController < ApplicationController
     end
   end
 
+  def search
+    @resources = Resource.all
+
+    if (params[:resource_type])
+      @resources = @resources.includes(:resource_type).where(resource_types: {name: params[:resource_type]})
+    end
+
+    respond_to do |format|
+      format.json {render json: {resources: @resources}}
+    end
+  end
+
 end
