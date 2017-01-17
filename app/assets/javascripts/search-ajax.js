ed18e2dc1027eef1df2aeb08a7c637fc25d9882c
O'Hoon Kwon <ohkwon@ucdavis.edu>
2017-01-17 01:18:29 -0800

    get code up to basis, finish when html is finished

--- app/assets/javascripts/search-ajax.js
+++ app/assets/javascripts/search-ajax.js
@@ -1,34 +1,22 @@
-var search = function () {
-  var data =
-
+var search = function (filter, div) {
+  $('body').css('cursor', 'progress');
+  var div = div;
+  var data = {resource_type: filter};
   $.ajax({
     type: "GET",
     url: "/search_filter",
-    data: {
-
-    },
+    data: data,
     success: function(data) {
-
+      var resources = data["resources"];
+      for(var i = 0; i < resources.length; i++) {
+        //insert formated html in a loop here when format exists
+      }
     },
     error: function(data) {
 
     },
     complete: function(data) {
-
+      $('body').css('cursor', 'default');
     }
   });
 };
-
-//reference, must delete
-
-script 
-
-$(".my_class").click(function(e){
-  var clickedElement = this;
-  var resourceType = this.html;
-  search(resourceType);
-})
-
-$("body").on("click", "my_class", function() {
-
-})
