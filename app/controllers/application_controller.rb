c56b84594366d543a152659937e825c231f3c603
Marisha Sahay <marishasahay@gmail.com>
2017-01-16 18:55:23 -0800

    After login redirect to resources page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    sign_in_url = "/about_us"
+    sign_in_url = "/"
     if request.referer == sign_in_url
       super
     else
-      stored_location_for(resource) || request.referer || "/resources"
+      stored_location_for(resource) || "/resources"
     end
   end
 
