92f2b3b327c0ac80a0cc5c14576eae60f11078cd
Marisha Sahay <marishasahay@gmail.com>
2017-01-16 16:02:54 -0800

    Redirect to resources after loggin in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    sign_in_url = "/about_us"
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || "/resources"
+    end
+  end
+
 end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,9 @@
+class RegistrationsController < Devise::SessionsController
+  
+  def new
+     self.resource = resource_class.new(sign_in_params)
+     store_location_for(resource, params[:redirect_to])
+     super
+  end
+  
+end
