d8816931b3f17fe65b27bb3577ceee2482874421
O'Hoon Kwon <ohkwon@ucdavis.edu>
2017-01-18 21:17:11 -0800

    mid process on creating query to merge multiple tables with filters

--- app/assets/javascripts/search-ajax.js
+++ app/assets/javascripts/search-ajax.js
@@ -1,15 +1,29 @@
-var search = function (filter, div) {
+var searchFunction = function (htmlElement, tableBody) {
   $('body').css('cursor', 'progress');
-  var div = div;
-  var data = {resource_type: filter};
+  var htmlElement = $(htmlElement);
+  var filterType = htmlElement.data("filter");
+  var input = { filters: [ [filterType, htmlElement.text()]] };
+  // data[:filters].concat([filterType, htmlElement]);
+  var tableBody = tableBody;
   $.ajax({
     type: "GET",
     url: "/search_filter",
-    data: data,
+    data: input,
     success: function(data) {
       var resources = data["resources"];
+      var newHtml = "";
+      debugger;
       for(var i = 0; i < resources.length; i++) {
-        //insert formated html in a loop here when format exists
+        newHtml += "<tr>";
+        newHtml += "<a href='/resources/" + resources[i]["id"] +"'>";
+        newHtml += "<td>" + resources[i]["title"] + "</td>";
+        newHtml += "</a>";
+        if (resources["world_region"]) {
+          newHtml += "<td>";
+          newHtml += resource["world_region"];
+          newHtml += "</td>";
+        }
+        newHtml += "</tr>";
       }
     },
     error: function(data) {
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,8 +7,11 @@ class SearchesController < ApplicationController
   def search
     @resources = Resource.all
 
-    if (params[:resource_type])
-      @resources = @resources.includes(:resource_type).where(resource_types: {name: params[:resource_type]})
+    if (params[:filters]["0"][0] == "resource_type")
+      @resources = @resources.select("resources.*, world_regions.name").joins(:world_regions).includes(:resource_type).where(resource_types: {name: params[:filters]["0"][1]})
+      binding.pry
+      @resources = @resources.includes(:resource_type).where(resource_types: {name: params[:filters]["0"][1]})
+      @resources = hash_create(@resources)
     end
 
     respond_to do |format|
@@ -16,4 +19,36 @@ class SearchesController < ApplicationController
     end
   end
 
+  private
+
+  def hash_create(resources)
+    resources_hash = []
+    @resources.each do |resource|
+      new_hash = {
+        abstract: null,
+        author: "Autumn Rauchwerk",
+        created_at: "2017-01-05T04:26:21.604Z",
+        date: resource.date,
+        description: resource.description,
+        id: resource.id,
+        is_problem: resource.is_problem,
+        is_published: resource.is_published,
+        is_solution: resource.is_solution,
+        publisher: resource.publisher,
+        source: resource.source,
+        title: resource.title,
+        url: resource.url,
+        world_region: resource.name
+      }
+    end
+  end
+
 end
+
+
+SELECT resources.*, world_regions.name, resource_types.*
+FROM "resources" 
+INNER JOIN "resources_world_regions" ON "resources_world_regions"."resource_id" = "resources"."id" 
+INNER JOIN "world_regions" ON "world_regions"."id" = "resources_world_regions"."world_region_id" 
+INNER JOIN "resource_types" ON "resource_types"."id" = "resources"."resource_type_id" 
+WHERE resource_types.name IN('Academic Article')
