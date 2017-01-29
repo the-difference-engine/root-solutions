5684334548d433df7490237ba0de5d268b1bae95
Andrealiz Afable <dreafable@gmail.com>
2017-01-28 19:12:49 -0800

    add code to html, add new file search.js

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,16 @@
+var status = "less";
+
+function toggleText()
+{
+    var text="Here is some text that I want added to the HTML file";
+    
+    if (status == "less") {
+        document.getElementById("textArea").innerHTML=text;
+        document.getElementById("toggleButton").innerText = "See Less";
+        status = "more";
+    } else if (status == "more") {
+        document.getElementById("textArea").innerHTML = "";
+        document.getElementById("toggleButton").innerText = "See More";
+        status = "less"
+    }
+}
