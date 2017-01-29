c6a53337d86e0b9ba7d3c115521603677d96e70f
Andrealiz Afable <dreafable@gmail.com>
2017-01-29 14:51:55 -0800

    change to jQuery truncate and read more/read less link, add control to search.js, add class to search html, add styling

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,16 +1,28 @@
-var status = "less";
-
-function toggleText()
-{
-    var text="Here is some text that I want added to the HTML file";
-    
-    if (status == "less") {
-        document.getElementById("textArea").innerHTML=text;
-        document.getElementById("toggleButton").innerText = "See Less";
-        status = "more";
-    } else if (status == "more") {
-        document.getElementById("textArea").innerHTML = "";
-        document.getElementById("toggleButton").innerText = "See More";
-        status = "less"
+$(document).ready(function() {
+  // Configure/customize these variables.
+  var showChar = 250;  // How many characters are shown by default
+  var ellipsestext = "...";
+  var moretext = "Read More";
+  var lesstext = "Read Less";
+  $('.more').each(function() {
+    var content = $(this).html();
+    if(content.length > showChar) {
+      var c = content.substr(0, showChar);
+      var h = content.substr(showChar, content.length - showChar);
+      var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+      $(this).html(html);
     }
-}
+  });
+  $(".morelink").click(function(){
+    if($(this).hasClass("less")) {
+      $(this).removeClass("less");
+      $(this).html(moretext);
+    } else {
+      $(this).addClass("less");
+      $(this).html(lesstext);
+    }
+    $(this).parent().prev().toggle();
+    $(this).prev().toggle();
+    return false;
+  });
+});
